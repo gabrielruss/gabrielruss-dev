c18b3a33faa26e1014611fe1ffc86ec1c6f1e81c
Gabriel <gabrielruss88@gmail.com>
2020-01-08 17:52:04 -0500

    day 9

--- components/Time.tsx
+++ components/Time.tsx
@@ -4,8 +4,12 @@ import TimeSince from './common/TimeSince';
 
 const Time = () => (
   <Container>
-    <Header type="h2">I have been a developer for give or take...</Header>
-    <TimeSince startDate={new Date(2015, 5)} />
+    <Header type="h2">🤓 I have been a developer for give or take...</Header>
+    <TimeSince startDate={new Date(2014, 8)} />
+    <Header type="h2">💍 married for </Header>
+    <TimeSince startDate={new Date(2011, 8)} />
+    <Header type="h2">👶 father for </Header>
+    <TimeSince startDate={new Date(2013, 8)} />
   </Container>
 );
 
--- components/common/TimeSince.tsx
+++ components/common/TimeSince.tsx
@@ -1,4 +1,6 @@
 import { FunctionComponent } from 'react';
+import { differenceInMonths } from 'date-fns';
+
 import Header from './Header';
 
 interface ITimeSinceProps {
@@ -6,17 +8,23 @@ interface ITimeSinceProps {
 }
 
 const TimeSince: FunctionComponent<ITimeSinceProps> = ({ startDate }) => {
-  const calculateTimeSince = () => {
-    const todayDateMonth = new Date(startDate).getMonth();
+  const months = differenceInMonths(new Date(), startDate);
+  const years = Math.floor(months / 12);
 
-    // TODO: get days since, then break it into even years
-    // then even months
-    // then days remaining
+  const remainingMonths = months - years * 12 + 1;
 
-    return todayDateMonth;
-  };
-
-  return <Header>{calculateTimeSince()}</Header>;
+  return (
+    <>
+      <div>
+        <Header>{years}</Header>
+        <p>years</p>
+      </div>
+      <div>
+        <Header>{remainingMonths}</Header>
+        <p>months</p>
+      </div>
+    </>
+  );
 };
 
 export default TimeSince;
