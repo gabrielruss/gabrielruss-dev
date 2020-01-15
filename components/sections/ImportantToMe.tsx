3bdce94c752d8194fa11f4dabdd8dbcda6bb06a4
Gabriel <gabrielruss88@gmail.com>
2020-01-14 19:06:37 -0500

    wording changes

--- components/sections/ImportantToMe.tsx
+++ components/sections/ImportantToMe.tsx
@@ -35,18 +35,17 @@ const ListItemContent = styled.p`
 const ImportantToMe = () => (
   <Container>
     <ImportantToMeWrapper>
-      <Header type="h2">What is important to me?</Header>
+      <Header type="h2">What coding practices are important to me?</Header>
       <ul>
         <li>
           <ListItemHeader>Pure, Single Purpose Functions</ListItemHeader>
           <ListItemContent>
-            When faced with a complex task, I try and break it down into the
-            smallest chunks possible until I can write individual methods for
-            each specific task.
+            Break complex tasks down into the smallest chunks possible until you
+            can write individual methods for each task.
           </ListItemContent>
           <ListItemContent>
-            I also try my best to ensure that given X input, the output will
-            always be Y. Then you have the perfect opportunity to write a test!
+            Try to ensure that given X input, the output will always be Y. Then
+            you have the perfect opportunity to write a test!
           </ListItemContent>
         </li>
         <li>
@@ -60,52 +59,39 @@ const ImportantToMe = () => (
         </li>
         <li>
           <ListItemHeader>
-            Would someone new to programming be able to follow your logic?
+            Could someone new to programming understand?
           </ListItemHeader>
           <ListItemContent>
-            A lot of times complex business logic can cause spaghetti code. And
-            while it might sound delicious, it will make you or someone else's
-            life difficult down the road.
-            <ListItemContent>
-              When things are starting to get crazy, try placing your functions
-              in the order in which they get called. If it makes sense, create a
-              controller method that has the order of operations for your
-              business logic.
-            </ListItemContent>
-            {/* Everyone loves to save time by naming
-            your variables quickly just to be able to tick the box. But keep in
-            mind that when you or someone else is reading your code 2 years from
-            now, will they understand WHY you did that? If the answer is maybe,
-            you should probably give it a better name and/or write a comment. */}
+            A lot of times complex business logic can cause spaghetti code.
+          </ListItemContent>
+          <ListItemContent>
+            Try placing your functions in the order in which they get called. If
+            it makes sense, create a controller method that has the order of
+            operations for your business logic.
           </ListItemContent>
         </li>
         <li>
           <ListItemHeader>Comment your code</ListItemHeader>
           <ListItemContent>
-            Ask yourself, when you or someone else is reading your code 2 years
-            from now, will they understand WHY you did that? If the answer is
-            maybe, you should probably write a comment.
+            When you or someone else reads your code years from now, will they
+            understand WHY you did that? If the answer is maybe, you should
+            probably write a comment.
           </ListItemContent>
           <ListItemContent>
-            If it is business logic, try writing the comment similar to a user
-            story. "The user should not be able to click Submit if I have not
-            filled out their birth date."
+            If it is business logic, write the comment similar to a user story.
+            "The user should not be able to click Submit if they have not filled
+            out their birth date."
           </ListItemContent>
         </li>
         <li>
-          <ListItemHeader>
-            Name everything in a way that your code comments seem redundant
-          </ListItemHeader>
+          <ListItemHeader>Use descriptive naming</ListItemHeader>
           <ListItemContent>
             Try not to use single letter variables. You may remember that "u"
             means "user", but someone else may not.
           </ListItemContent>
           <ListItemContent>
-            Ok so this one is also a bit of an art. You can't exactly name all
-            your variables like "shouldTheUserNOTBeAbleToClickTheSubmitButton".
-            But I would argue that "disableSubmitButton" is always better than
-            simply "disable". Be as brief as possible while still describing
-            what it does.
+            "disableSubmitButton" is always better than simply "disable". Be as
+            brief as possible while still describing what it does.
           </ListItemContent>
         </li>
         <li>
