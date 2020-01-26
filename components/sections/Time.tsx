c57c0f69a2c7288dfb1e3f6fb163f62aa28edbe3
Gabriel <gabrielruss88@gmail.com>
2020-01-25 20:12:11 -0500

    day 26

--- components/sections/Time.tsx
+++ components/sections/Time.tsx
@@ -1,5 +1,4 @@
-import Header from '../common/Header';
-import Container from '../common/Container';
+import Header, { StyledHeader } from '../common/Header';
 import TimeSince from '../common/TimeSince';
 import { styled } from '..';
 
@@ -8,43 +7,58 @@ const TimeWrapper = styled.div`
   align-items: center;
   justify-content: center;
   text-align: center;
-  padding: 10rem 3rem;
+  min-height: 100vh;
+  gap: 2rem;
+  /* padding: 10rem 3rem; */
   /* box-shadow: 0px 5px 5px ${props => props.theme.colors.trans_black} */
     /* 0px -5px 5px ${props => props.theme.colors.trans_black}; */
+
+ 
+  
 `;
 
 const TimeHeader = styled.div`
-  max-width: ${props => props.theme.break_large};
+  @media (max-width: ${props => props.theme.break_small}) {
+    padding: 0 10rem;
+  }
 `;
 
 const TimeContent = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(46rem, 1fr));
-  gap: 5rem;
+  grid-template-columns: repeat(auto-fit, minmax(36rem, 1fr));
+  gap: 2rem;
 `;
 
 const Time = () => (
   <TimeWrapper>
     <TimeHeader>
       <Header size="medium">
-        🤓 i have been a developer for give or take...
+        🤓
+        <br /> i have been a developer for give or take...
       </Header>
       <TimeSince startDate={new Date(2014, 7)} />
     </TimeHeader>
     <TimeContent>
       <div>
-        <Header size="medium">👶 father for </Header>
+        <Header size="medium">
+          👶
+          <br /> father for{' '}
+        </Header>
         <TimeSince startDate={new Date(2013, 8)} />
       </div>
       <div>
-        <Header size="medium">💍 married for </Header>
+        <Header size="medium">
+          💍
+          <br /> married for{' '}
+        </Header>
         <TimeSince startDate={new Date(2011, 8)} />
       </div>
     </TimeContent>
-    <span>By the way...that isn't an error in months...</span>
-    <span>
+    {/* <p>
+      By the way...that isn't an error in months...
+      <br />
       Major events just seem to happen around August in my life for some reason
-    </span>
+    </p> */}
   </TimeWrapper>
 );
 
