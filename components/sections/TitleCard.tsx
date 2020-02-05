2af88d8d3860ae9607124dc36644d1a5129d85c5
Gabriel <gabrielruss88@gmail.com>
2020-02-04 19:32:20 -0500

    36

--- components/sections/TitleCard.tsx
+++ components/sections/TitleCard.tsx
@@ -2,52 +2,73 @@ import { styled } from '..';
 import StyledBorderContainer from '../styles/StyledBorderContainer';
 import Header from '../common/Header';
 
-export const StyledTitleCard = styled.div`
-  display: flex;
-  flex-flow: column;
-  text-align: center;
-  transition: margin-top 0.2s ease;
-  margin-top: 25vh;
-  height: 100vh;
+const LongSquare = styled.div`
+  z-index: 0;
+  background-color: ${props => props.theme.colors.teal};
+
+  /*  1920 */
+  top: 3%;
+  left: 21%;
+  height: calc(200px + (300 - 200) * ((100vw - 360px) / (1920 - 360)));
+  width: calc(600px + (900 - 600) * ((100vw - 360px) / (1920 - 360)));
 
   @media (max-width: ${props => props.theme.break_large}) {
-    margin-top: 20vh;
+    top: 3%;
+    left: 17%;
+    height: calc(250px + (300 - 250) * ((100vw - 360px) / (1920 - 360)));
+    width: calc(600px + (775 - 600) * ((100vw - 360px) / (1920 - 360)));
   }
 
   @media (max-width: ${props => props.theme.break_medium}) {
-    margin-top: 15vh;
-  }
-
-  ${StyledBorderContainer} {
-    position: relative;
-    z-index: 1;
+    top: 3%;
+    left: 17%;
+    height: calc(220px + (300 - 220) * ((100vw - 360px) / (1920 - 360)));
+    width: 75vw;
+    /* width: calc(600px + (750 - 600) * ((100vw - 360px) / (1920 - 360))); */
   }
 `;
 
-const LongSquare = styled.div`
-  z-index: 0;
+const LongSquareContainer = styled.div`
   position: relative;
-  /* height: 30rem;
-  width: 92rem; */
-  height: calc(150px + (350 - 150) * ((100vw - 360px) / (1920 - 360)));
-  width: calc(300px + (950 - 300) * ((100vw - 360px) / (1920 - 360)));
-  margin-left: 35rem;
-  margin-top: -25rem;
-  background-color: ${props => props.theme.colors.teal};
 
-  /* calc(
-        80px + (150 - 80) *
-          ((100vw - 360px) / (1920 - 360))
-      ); */
+  ${StyledBorderContainer} {
+    z-index: 1;
+  }
+
+  ${LongSquare} {
+    position: absolute;
+  }
+`;
+
+export const StyledTitleCard = styled.div`
+  ${LongSquareContainer} {
+    display: flex;
+    flex-flow: column;
+    text-align: center;
+    transition: margin-top 0.2s ease;
+    margin-top: 25vh;
+    height: 100vh;
+
+    @media (max-width: ${props => props.theme.break_large}) {
+      margin-top: 20vh;
+    }
+
+    @media (max-width: ${props => props.theme.break_medium}) {
+      margin-top: 15vh;
+    }
+  }
 `;
 
 const TitleCard = () => (
   <StyledTitleCard>
-    <StyledBorderContainer>
-      <Header>Gabriel Russ</Header>
-      <p>i’m just a guy who’s a web developer for fun</p>
-    </StyledBorderContainer>
-    <LongSquare />
+    <LongSquareContainer>
+      <StyledBorderContainer>
+        <Header>Gabriel Russ</Header>
+        <p>i’m just a guy who’s a web developer for fun</p>
+      </StyledBorderContainer>
+      <LongSquare />
+      {/* <LongSquare /> */}
+    </LongSquareContainer>
   </StyledTitleCard>
 );
 
