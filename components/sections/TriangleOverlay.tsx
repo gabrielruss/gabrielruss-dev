6683f3ece3f0ee91826b157d4fc2261225a0a997
Gabriel <gabrielruss88@gmail.com>
2020-02-13 19:31:39 -0500

    day 45

--- components/sections/TriangleOverlay.tsx
+++ components/sections/TriangleOverlay.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { styled } from '..';
 import RandomTriangle, { TriangleColors } from '../styles/RandomTriangle';
 import { randomNumberPlease } from '../utilities/_helpers';
+import useInterval from '../hooks/useInterval';
 
 interface IStyledTriangleOverlay {
   triangles: number;
@@ -31,69 +32,23 @@ const TriangleColorArray: TriangleColors[] = [
   'black',
 ];
 
-/**
- *
- * @param baseNumber max of 50
- */
-function getInitialTriangles(baseNumber: number) {
-  if (baseNumber > 50) {
-    baseNumber = 50;
-  }
-
-  const triangleArray: Array<JSX.Element> = [];
-
-  for (let i = 0; i <= baseNumber; i++) {
-    triangleArray.push(
-      <RandomTriangle
-        top={randomNumberPlease(100, 5)}
-        right={randomNumberPlease(90, 0)}
-        rotation={Math.random() < 0.5 ? '' : 'reverse'}
-        width={randomNumberPlease(105, 85)}
-        height={randomNumberPlease(75, 55)}
-        opacity={randomNumberPlease(65, 35) / 100}
-        color={TriangleColorArray[randomNumberPlease(5, 0)]}
-        key={i}
-      />
-    );
-  }
-
-  // maximum of baseNumber
-  // spawn 1 out after 5 seconds
-  // spawn another in after speed
-
-  // ? make them clickable and when clicked they dissapear?
-
-  return triangleArray;
-}
-
 export function TriangleOverlay({
   children,
   speed = 5000,
   baseTriangles = 10,
 }) {
-  const [triangles, setTriangles] = useState<Array<JSX.Element>>(
+  const [count, setCount] = useState(baseTriangles + 1);
+
+  const [triangles, setTriangles] = useState(() =>
     getInitialTriangles(baseTriangles)
   );
 
-  let count = baseTriangles;
+  useInterval(() => {
+    setCount(count + 1);
 
-  // TODO: this works, but i need it to transition out
-  // I think i need to transition triangles out in 2 steps
-  // 1. set triangle[0] to be opacity 0 which will show the transition out
-  // 2. push new triangle with opacity 0
-  // 3. remove triangle[0] from the array
-  // 4. set triangle[max] to opacity 1
-  const triangleInterchange = () => {
-    if (triangles) {
-      count = count + 1;
+    setTriangles(prevTriangles => {
+      const newTriangles = prevTriangles.slice(1);
 
-      // todo: figure out how to remove 0, and add in the last
-      // something is making it only add the last.
-      // try not spreading in a new array?
-      const newTriangles = [...triangles];
-      // remove one from front
-      newTriangles.shift();
-      // add new to back
       newTriangles.push(
         <RandomTriangle
           top={randomNumberPlease(100, 5)}
@@ -106,19 +61,42 @@ export function TriangleOverlay({
           key={count}
         />
       );
-      setTriangles(newTriangles);
-    }
-  };
+      return newTriangles;
+    });
+  }, speed);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      triangleInterchange();
-    }, speed);
-    return () => clearInterval(interval);
-  }, []);
+  /**
+   *
+   * @param baseNumber max of 50
+   */
+  function getInitialTriangles(baseNumber: number) {
+    if (baseNumber > 50) {
+      baseNumber = 50;
+    }
+
+    const triangleArray: Array<JSX.Element> = [];
+
+    for (let i = 0; i <= baseNumber; i++) {
+      triangleArray.push(
+        <RandomTriangle
+          top={randomNumberPlease(100, 5)}
+          right={randomNumberPlease(90, 0)}
+          rotation={Math.random() < 0.5 ? '' : 'reverse'}
+          width={randomNumberPlease(105, 85)}
+          height={randomNumberPlease(75, 55)}
+          opacity={randomNumberPlease(65, 35) / 100}
+          color={TriangleColorArray[randomNumberPlease(5, 0)]}
+          key={i}
+        />
+      );
+    }
+
+    return triangleArray;
+  }
 
   return (
     <StyledTriangleOverlay triangles={baseTriangles}>
+      <>{count}</>
       {triangles}
       {children}
     </StyledTriangleOverlay>
