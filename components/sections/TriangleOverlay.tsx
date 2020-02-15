2c4c3f67ffa4c169d516221c14a5c7e8a97cf71f
Gabriel <gabrielruss88@gmail.com>
2020-02-14 20:22:12 -0500

    day 46

--- components/sections/TriangleOverlay.tsx
+++ components/sections/TriangleOverlay.tsx
@@ -16,11 +16,11 @@ const StyledTriangleOverlay = styled.div<IStyledTriangleOverlay>`
   }
 
   ${RandomTriangle}:first-of-type {
-    visibility: hidden;
+    opacity: 0;
   }
 
   ${RandomTriangle}:nth-of-type(${props => props.triangles + 1}) {
-    visibility: hidden;
+    opacity: 0;
   }
 `;
 
@@ -47,20 +47,29 @@ export function TriangleOverlay({
     setCount(count + 1);
 
     setTriangles(prevTriangles => {
-      const newTriangles = prevTriangles.slice(1);
+      let newTriangles: JSX.Element[] = [...prevTriangles];
 
-      newTriangles.push(
-        <RandomTriangle
-          top={randomNumberPlease(100, 5)}
-          right={randomNumberPlease(90, 0)}
-          rotation={Math.random() < 0.5 ? '' : 'reverse'}
-          width={randomNumberPlease(105, 85)}
-          height={randomNumberPlease(75, 55)}
-          opacity={randomNumberPlease(65, 35) / 100}
-          color={TriangleColorArray[randomNumberPlease(5, 0)]}
-          key={count}
-        />
-      );
+      if (count % 2 !== 0) {
+        // first run
+        console.log('adding triangle');
+        newTriangles.push(
+          <RandomTriangle
+            top={randomNumberPlease(100, 5)}
+            right={randomNumberPlease(90, 0)}
+            rotation={Math.random() < 0.5 ? '' : 'reverse'}
+            width={randomNumberPlease(105, 85)}
+            height={randomNumberPlease(75, 55)}
+            opacity={randomNumberPlease(65, 35) / 100}
+            color={TriangleColorArray[randomNumberPlease(5, 0)]}
+            key={count}
+          />
+        );
+      } else {
+        // second run
+        console.log('removing triangle');
+        newTriangles = prevTriangles.slice(1);
+      }
+
       return newTriangles;
     });
   }, speed);
@@ -94,9 +103,25 @@ export function TriangleOverlay({
     return triangleArray;
   }
 
+  // a list of triangle ids or keys
+  // triangleIds = [0, 1, 2, 3, 4]
+  // transparent = null
+  // every 5 seconds, pick 1 random number from the list of triangles and set to state
+  // transparent = 1
+  // on each triangle opacity={key === toDelete ? 0 : randomNumberPlease(65, 35) / 100}
+  // this will fade out the triangle
+  // 5 seconds pass
+  // check to see if there is a number in transparent, if so, remove it from list of triangles
+  // reset transparent
+  // transparent = null
+
+  // 5 seconds pass
+  // loops through all again
+  // pick 1 random number from the list of triangles and set to state
+  // transparent = 4
+
   return (
     <StyledTriangleOverlay triangles={baseTriangles}>
-      <>{count}</>
       {triangles}
       {children}
     </StyledTriangleOverlay>
