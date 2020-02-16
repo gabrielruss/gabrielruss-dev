19805348cb18eb15580b91794c7fe49befa78f60
Gabriel <gabrielruss88@gmail.com>
2020-02-15 21:04:27 -0500

    day 47

--- components/sections/TriangleOverlay.tsx
+++ components/sections/TriangleOverlay.tsx
@@ -15,13 +15,13 @@ const StyledTriangleOverlay = styled.div<IStyledTriangleOverlay>`
     transition: 1s all ease;
   }
 
-  ${RandomTriangle}:first-of-type {
+  /* ${RandomTriangle}:first-of-type {
     opacity: 0;
   }
 
   ${RandomTriangle}:nth-of-type(${props => props.triangles + 1}) {
     opacity: 0;
-  }
+  } */
 `;
 
 const TriangleColorArray: TriangleColors[] = [
@@ -34,74 +34,90 @@ const TriangleColorArray: TriangleColors[] = [
 
 export function TriangleOverlay({
   children,
-  speed = 5000,
+  speed = 1000,
   baseTriangles = 10,
 }) {
-  const [count, setCount] = useState(baseTriangles + 1);
+  // const [count, setCount] = useState(baseTriangles + 1);
 
-  const [triangles, setTriangles] = useState(() =>
-    getInitialTriangles(baseTriangles)
-  );
+  // const [triangles, setTriangles] = useState(() =>
+  //   getInitialTriangles(baseTriangles)
+  // );
 
-  useInterval(() => {
-    setCount(count + 1);
+  const [triangleIds, setTriangleIds] = useState<number[]>(() => {
+    const initialTriangleIds = [];
 
-    setTriangles(prevTriangles => {
-      let newTriangles: JSX.Element[] = [...prevTriangles];
-
-      if (count % 2 !== 0) {
-        // first run
-        console.log('adding triangle');
-        newTriangles.push(
-          <RandomTriangle
-            top={randomNumberPlease(100, 5)}
-            right={randomNumberPlease(90, 0)}
-            rotation={Math.random() < 0.5 ? '' : 'reverse'}
-            width={randomNumberPlease(105, 85)}
-            height={randomNumberPlease(75, 55)}
-            opacity={randomNumberPlease(65, 35) / 100}
-            color={TriangleColorArray[randomNumberPlease(5, 0)]}
-            key={count}
-          />
-        );
-      } else {
-        // second run
-        console.log('removing triangle');
-        newTriangles = prevTriangles.slice(1);
-      }
-
-      return newTriangles;
-    });
-  }, speed);
-
-  /**
-   *
-   * @param baseNumber max of 50
-   */
-  function getInitialTriangles(baseNumber: number) {
-    if (baseNumber > 50) {
-      baseNumber = 50;
+    for (let i = 0; i <= baseTriangles; i++) {
+      initialTriangleIds.push(i);
     }
 
-    const triangleArray: Array<JSX.Element> = [];
+    return initialTriangleIds;
+  });
 
-    for (let i = 0; i <= baseNumber; i++) {
-      triangleArray.push(
-        <RandomTriangle
-          top={randomNumberPlease(100, 5)}
-          right={randomNumberPlease(90, 0)}
-          rotation={Math.random() < 0.5 ? '' : 'reverse'}
-          width={randomNumberPlease(105, 85)}
-          height={randomNumberPlease(75, 55)}
-          opacity={randomNumberPlease(65, 35) / 100}
-          color={TriangleColorArray[randomNumberPlease(5, 0)]}
-          key={i}
-        />
-      );
-    }
+  const [transparentTriangle, setTransparentTriangle] = useState();
 
-    return triangleArray;
-  }
+  // useInterval(() => {
+  //   const trianglePick = randomNumberPlease(0, baseTriangles + 1);
+  //   setTransparentTriangle(trianglePick);
+  // }, speed);
+  // useInterval(() => {
+  //   setCount(count + 1);
+
+  //   setTriangles(prevTriangles => {
+  //     let newTriangles: JSX.Element[] = [...prevTriangles];
+
+  //     if (count % 2 !== 0) {
+  //       // first run
+  //       console.log('adding triangle');
+  //       newTriangles.push(
+  //         <RandomTriangle
+  //           top={randomNumberPlease(100, 5)}
+  //           right={randomNumberPlease(90, 0)}
+  //           rotation={Math.random() < 0.5 ? '' : 'reverse'}
+  //           width={randomNumberPlease(105, 85)}
+  //           height={randomNumberPlease(75, 55)}
+  //           opacity={randomNumberPlease(65, 35) / 100}
+  //           color={TriangleColorArray[randomNumberPlease(5, 0)]}
+  //           key={count}
+  //         />
+  //       );
+  //     } else {
+  //       // second run
+  //       console.log('removing triangle');
+  //       newTriangles = prevTriangles.slice(1);
+  //     }
+
+  //     return newTriangles;
+  //   });
+  // }, speed);
+
+  // /**
+  //  *
+  //  * @param baseNumber max of 50
+  //  */
+  // function getInitialTriangles(baseNumber: number) {
+  //   if (baseNumber > 50) {
+  //     baseNumber = 50;
+  //   }
+
+  //   const triangleArray: Array<JSX.Element> = [];
+
+  //   for (let i = 0; i <= baseNumber; i++) {
+  //     triangleArray.push(
+  //       <RandomTriangle
+  //         top={randomNumberPlease(100, 5)}
+  //         right={randomNumberPlease(90, 0)}
+  //         rotation={Math.random() < 0.5 ? '' : 'reverse'}
+  //         width={randomNumberPlease(105, 85)}
+  //         height={randomNumberPlease(75, 55)}
+  //         opacity={randomNumberPlease(65, 35) / 100}
+  //         color={TriangleColorArray[randomNumberPlease(5, 0)]}
+  //         key={i}
+  //       />
+  //     );
+  //   }
+
+  //   return triangleArray;
+  // }
 
   // a list of triangle ids or keys
   // triangleIds = [0, 1, 2, 3, 4]
@@ -122,7 +138,23 @@ export function TriangleOverlay({
 
   return (
     <StyledTriangleOverlay triangles={baseTriangles}>
-      {triangles}
+      <>{transparentTriangle}</>
+      {triangleIds.map(t => (
+        <RandomTriangle
+          top={randomNumberPlease(100, 5)}
+          right={randomNumberPlease(90, 0)}
+          rotation={Math.random() < 0.5 ? '' : 'reverse'}
+          width={randomNumberPlease(105, 85)}
+          height={randomNumberPlease(75, 55)}
+          opacity={
+            randomNumberPlease(0, baseTriangles + 1) === t
+              ? 0
+              : randomNumberPlease(65, 35) / 100
+          }
+          color={TriangleColorArray[randomNumberPlease(5, 0)]}
+          key={t}
+        />
+      ))}
       {children}
     </StyledTriangleOverlay>
   );
