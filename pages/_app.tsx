5b6a28849477df772acf3069b7a732f2b11d5114
Gabriel <gabrielruss88@gmail.com>
2020-07-09 12:53:04 -0400

    adding meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../components';
 import Page from '../components/Page';
+import Meta from '../components/common/Meta';
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
+        <Meta />
         <Page>
           <Component {...pageProps} />
         </Page>
