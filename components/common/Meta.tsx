08cfef9d9fb8b2cdf6dd0d085286cd2e046e2a2b
Gabriel <gabrielruss88@gmail.com>
2020-07-21 17:42:22 -0400

    trying again

--- components/common/Meta.tsx
+++ components/common/Meta.tsx
@@ -2,20 +2,31 @@ import Head from 'next/head';
 
 const Meta = () => (
   <Head>
+    <title>Gabriel Russ Portfolio</title>
+    <meta name="title" content="Gabriel Russ Portfolio" />
+    <meta name="description" content="" />
+    <link rel="shortcut icon" href="/static/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
+    {/* Open Graph */}
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://gabrielruss.dev/" />
     <meta property="og:title" content="Gabriel Russ Portfolio" />
     <meta
       property="og:description"
       content="making the internet a better place, one div at a time"
     />
     <meta property="og:image" content="https://imgur.com/wUZp9FX" />
-    <meta property="og:url" content="https://gabrielruss.dev" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content="https://imgur.com/wUZp9FX" />
-    <meta name="twitter:image:alt" content="Gabriel Russ Portfolio" />
-    <link rel="shortcut icon" href="/static/favicon.ico" />
-    <title>Gabriel Russ Portfolio</title>
+    {/* Twitter */}
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://gabrielruss.dev/" />
+    <meta property="twitter:title" content="Gabriel Russ Portfolio" />
+    <meta
+      property="twitter:description"
+      content="making the internet a better place, one div at a time"
+    />
+    <meta property="twitter:image" content="https://imgur.com/wUZp9FX" />
+    <meta property="twitter:image:alt" content="Gabriel Russ Portfolio" />
   </Head>
 );
 
