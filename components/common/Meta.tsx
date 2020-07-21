58af98fa17db603c586b02615985debdbeecd49a
Gabriel <gabrielruss88@gmail.com>
2020-07-21 17:19:14 -0400

    twitter image

--- components/common/Meta.tsx
+++ components/common/Meta.tsx
@@ -12,7 +12,10 @@ const Meta = () => (
     <meta property="og:image" content="/static/biglogo.png" />
     <meta property="og:url" content="https://gabrielruss.dev" />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content="/static/biglogo.png" />
+    <meta
+      name="twitter:image"
+      content="https://gabrielruss-dev-90tyfk86b.vercel.app/static/biglogo.png"
+    />
     <meta name="twitter:image:alt" content="Gabriel Russ Portfolio" />
     <link rel="shortcut icon" href="/static/favicon.ico" />
     <title>Gabriel Russ Portfolio</title>
