08ae30bc64e6405690dd16112ea9993007ddecb8
Gabriel <gabrielruss88@gmail.com>
2020-07-21 17:09:53 -0400

    updating meta description

--- components/common/Meta.tsx
+++ components/common/Meta.tsx
@@ -7,7 +7,7 @@ const Meta = () => (
     <meta property="og:title" content="Gabriel Russ Portfolio" />
     <meta
       property="og:description"
-      content="making the internet a better place, one <div> at a time"
+      content="making the internet a better place, one div at a time"
     />
     <meta property="og:image" content="/static/biglogo.png" />
     <meta property="og:url" content="https://gabrielruss.dev" />
