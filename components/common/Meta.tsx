aad7c290bedd76dc7c35c7da1699573b089a2a35
Gabriel <gabrielruss88@gmail.com>
2020-07-21 17:08:13 -0400

    adding meta tags

--- components/common/Meta.tsx
+++ components/common/Meta.tsx
@@ -4,8 +4,17 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
+    <meta property="og:title" content="Gabriel Russ Portfolio" />
+    <meta
+      property="og:description"
+      content="making the internet a better place, one <div> at a time"
+    />
+    <meta property="og:image" content="/static/biglogo.png" />
+    <meta property="og:url" content="https://gabrielruss.dev" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:image:alt" content="Gabriel Russ Portfolio" />
     <link rel="shortcut icon" href="/static/favicon.ico" />
-    <title>Gabriel Russ</title>
+    <title>Gabriel Russ Portfolio</title>
   </Head>
 );
 
