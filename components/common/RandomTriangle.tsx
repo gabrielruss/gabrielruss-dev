2e6b12152adc5dd2619e2d12ce33fcab980e2897
Gabriel Russ <8887997+gabrielruss@users.noreply.github.com>
2020-10-28 09:48:55 -0400

    Turning off timer until fix (#8)

--- components/common/RandomTriangle.tsx
+++ components/common/RandomTriangle.tsx
@@ -63,10 +63,10 @@ const RandomTriangleStyle = styled.div<StyledRandomTriangleProps>`
       transform: rotate(360deg);
     }
   }
-
+  /* 
   :hover {
     cursor: pointer;
-  }
+  } */
 `;
 
 interface RandomTriangleModel {
@@ -90,19 +90,19 @@ function RandomTriangle({ speed }: RandomTriangleModel) {
     getRandomProps()
   );
 
-  useInterval(() => {
-    const trianglePick1 = randomNumberPlease(15, 1);
-    const trianglePick2 = randomNumberPlease(15, 1);
+  // useInterval(() => {
+  //   const trianglePick1 = randomNumberPlease(15, 1);
+  //   const trianglePick2 = randomNumberPlease(15, 1);
 
-    if (trianglePick1 === trianglePick2) {
-      setRandomProps(getRandomProps());
-    }
-  }, speed);
+  //   if (trianglePick1 === trianglePick2) {
+  //     setRandomProps(getRandomProps());
+  //   }
+  // }, speed);
 
   return (
     <RandomTriangleStyle
       {...randomProps}
-      onClick={() => setRandomProps(getRandomProps())}
+      // onClick={() => setRandomProps(getRandomProps())}
     />
   );
 }
