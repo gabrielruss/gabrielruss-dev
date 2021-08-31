90a378bdac71a885428e209731ed0ef628185067
Gabriel <gabrielruss88@gmail.com>
2021-08-31 16:38:03 -0400

    new resume

--- components/util/_constants.ts
+++ components/util/_constants.ts
@@ -1,4 +1,4 @@
-const RESUME_GUID = '1RRFL0vpvlihysuM0nF8Sy3xEcvCoVXya';
+const RESUME_GUID = '1jvNDoolVt6iWI1Jv54AtFWTp4AAv_Iey';
 
 export const RESUME_DL_LINK = `https://drive.google.com/uc?export=download&id=${RESUME_GUID}`;
 
